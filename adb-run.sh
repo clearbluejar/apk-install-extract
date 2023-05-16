07e483aa75eb48c21fbc85a2fb5fbee04467aba8
clearbluejar <3752074+clearbluejar@users.noreply.github.com>
2023-05-16 13:23:16 -0400

    another09

--- adb-run.sh
+++ adb-run.sh
@@ -3,7 +3,4 @@ ver=$(aapt dump badging example.apk | awk -F " " '/package/ {print $4}' | awk -F
 act=$(aapt dump badging $1|awk -F" " '/launchable-activity/ {print $2}'|awk -F"'" '/name=/ {print $2}')
 pkg_ver=$( echo "$pkg.$ver")
 echo "Running package: $pkg ver: $ver with activity: $act"
-echo "$pkg_ver"
-aapt dump badging $1 > $pkg_ver.badging.txt
-
 adb shell am start -n $pkg/$act
