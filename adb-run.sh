1d25a52cbff21a9f32f9506dda060e78af54867c
clearbluejar <3752074+clearbluejar@users.noreply.github.com>
2023-06-30 10:05:42 -0400

    Update adb-run.sh
    fix log ver

--- adb-run.sh
+++ adb-run.sh
@@ -1,5 +1,5 @@
 pkg=$(aapt dump badging $1|awk -F" " '/package/ {print $2}'|awk -F"'" '/name=/ {print $2}')
-ver=$(aapt dump badging example.apk | awk -F " " '/package/ {print $4}' | awk -F "'" '/versionName=/ {print $2}')
+ver=$(aapt dump badging $1 | awk -F " " '/package/ {print $4}' | awk -F "'" '/versionName=/ {print $2}')
 act=$(aapt dump badging $1|awk -F" " '/launchable-activity/ {print $2}'|awk -F"'" '/name=/ {print $2}')
 pkg_ver=$( echo "$pkg.$ver")
 echo "Running package: $pkg ver: $ver with activity: $act"
